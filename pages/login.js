5207667bad748740ece382f005e20c4ca6576eeb
zalfanuramalia <zalfafanur@gmail.com>
2022-03-16 23:00:22 +0700

    chore: edited pages

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,6 @@
 import {Col, Row, Container} from 'react-bootstrap'
 import Image from 'next/image'
 import Link from 'next/link'
-import FaMailBulk from 'react-icons/fa'
 
 const login = () => {
     return (
