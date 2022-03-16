de3e039363968e1da1c05a4fbbe34ec1e5094bfc
zalfanuramalia <zalfafanur@gmail.com>
2022-03-16 23:40:58 +0700

    feat: init signup pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
+import '../styles/signup.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import {useEffect} from 'react'
 
 function MyApp({ Component, pageProps }) {
